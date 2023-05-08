d7a6e2423c0959c0546ef83422c02ff623098b25
YehTH <yth1303@icloud.com>
2023-05-08 21:11:59 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello There!</h1>
+      <p> I cant fit everything in the whiteboard so here it is:
+        My name is TH and I am a SST student in guitar ensemble!
+      </p>
     </div>
   );
 }
